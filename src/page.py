f70eca7b5d13dd3765ae023e22a74b1c817c9144
Carine Dengler <git@carine-dengler.de>
2018-08-28 22:11:29 +0200

    page.py
    - added toc
    - added search for section

--- src/page.py
+++ src/page.py
@@ -21,6 +21,7 @@
 
 
 # standard library imports
+import json
 
 # third party imports
 
@@ -73,7 +74,10 @@ class Page(object):
             section = self.sections
             value = self.find_paragraphs(section=section)
         elif name == "pretty":
-            value = self._find_pretty()
+            value = self.find_pretty()
+        elif name == "toc":
+            section = self.sections
+            value = self.find_toc(section)
         else:
             msg = "does not have attribute '{}'".format(name)
             raise RuntimeError(msg)
@@ -124,7 +128,7 @@ class Page(object):
             raise RuntimeError(msg.format(exception))
         return paragraphs
 
-    def _find_pretty(self):
+    def find_pretty(self):
         """Find pretty print.
 
         :returns: pretty
@@ -148,3 +152,81 @@ class Page(object):
             msg = "failed to find pretty print\t: {}"
             raise RuntimeError(msg.format(exception))
         return pretty
+
+    def _find_toc(self, section, level=1):
+        """Find table of contents.
+
+        :param Section section: section
+        :param int level: level
+
+        :returns: table of contents
+        :rtype: str
+        """
+        try:
+            if level == 5:
+                toc = {
+                    section.heading: [
+                        {subsection.heading: []}
+                        for subsection in section.subsections
+                    ]
+                }
+                return toc
+            else:
+                subsections = [
+                    self._find_toc(subsection, level=level+1)
+                    for subsection in section.subsections
+                ]
+                toc = {section.heading: subsections}
+                return toc
+        except Exception as exception:
+            msg = "failed to find table of contents\t: {}"
+            raise RuntimeError(msg.format(exception))
+
+    def find_toc(self, section, level=1, file_format="json"):
+        """Find table of contents.
+
+        :param Section section: section
+        :param int level: level
+        :param str file_format: file format
+
+        :returns: table of contents
+        :rtype: str
+        """
+        try:
+            toc = self._find_toc(section, level=level)
+            if file_format == "json":
+                toc = json.dumps(toc)
+            else:
+                msg = "file format {} is not supported".format(file_format)
+                raise RuntimeError(msg)
+        except RuntimeError:
+            raise
+        except Exception as exception:
+            msg = "failed to find table of contents {}".format(exception)
+            raise RuntimeError(msg)
+        return toc
+
+    def find_section(self, heading):
+        """Find section.
+
+        :param str heading: heading
+
+        :returns: section
+        :rtype: Section
+        """
+        try:
+            section = self.sections
+            stack = [section]
+            while stack:
+                section = stack.pop(0)
+                if section.heading == heading:
+                    break
+                else:
+                    for subsection in section.subsections:
+                        stack.insert(0, subsection)
+            else:
+                section = None
+        except Exception as exception:
+            msg = "failed to find section {}".format(exception)
+            raise RuntimeError(msg)
+        return section
