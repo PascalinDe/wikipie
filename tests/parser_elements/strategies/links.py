2182e61e015b68bf43ad152edd2ea574e10e8430
Carine Dengler <git@carine-dengler.de>
2018-09-16 21:11:38 +0200

    added tests and strategies

---
+++ tests/parser_elements/strategies/links.py
@@ -0,0 +1,167 @@
+#    This file is part of WikiPie 1.0.
+#    Copyright (C) 2018  Carine Dengler
+#
+#    WikiPie is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+:synopsis: Links wikitext generation.
+"""
+
+
+# standard library imports
+import csv
+import string
+
+# third party imports
+import hypothesis
+import hypothesis.strategies
+
+# library specific imports
+
+
+@hypothesis.strategies.composite
+def namespace(draw):
+    """Return namespace.
+
+    namespace = any namespace;
+
+    :returns: namespace
+    :rtype: str
+    """
+    with open("tests/parser_elements/data/namespaces.csv") as fp:
+        reader = csv.reader(fp)
+        namespaces = next(reader)
+    namespace_ = draw(hypothesis.strategies.sampled_from(namespaces))
+    return namespace_
+
+
+@hypothesis.strategies.composite
+def page_name(draw, min_size, max_size):
+    """Return page_name.
+
+    :param int min_size: minimum size
+    :param int max_size: maximum size
+
+    page_name = printable w/o "#:<>[]_{|}", { printable w/o "#<>[]_{|}" };
+
+    :returns: page_name
+    :rtype: str
+    """
+    initChars = "".join(
+        char for char in string.printable if char not in "#:<>[]_{|}"
+    )
+    initChar = draw(hypothesis.strategies.sampled_from(initChars))
+    bodyChars = "".join(
+        char for char in string.printable if char not in "#<>[]_{|}"
+    )
+    body = draw(
+        hypothesis.strategies.text(
+            alphabet=bodyChars, min_size=min_size, max_size=max_size
+        )
+    )
+    page_name_ = initChar + body
+    return page_name_
+
+
+@hypothesis.strategies.composite
+def anchor(draw, min_size, max_size):
+    """Return anchor.
+
+    :param int min_size: minimum size
+    :param int max_size: maximum size
+
+    anchor = { printable w/o "#<>[]_{|}" }-;
+
+    :returns: anchor
+    :rtype: str
+    """
+    bodyChars = "".join(
+        char for char in string.printable if char not in "#<>[]_{|}"
+    )
+    anchor_ = draw(
+        hypothesis.strategies.text(
+            alphabet=bodyChars,
+            min_size=min_size,
+            max_size=max_size
+        )
+    )
+    return anchor_
+
+
+@hypothesis.strategies.composite
+def word_ending(draw, min_size, max_size):
+    """Return word_ending.
+
+    :param int min_size: minimum size
+    :param int max_size: maximum size
+
+    word_ending = { ascii_letter }-;
+
+    :returns: word_ending
+    :rtype: str
+    """
+    word_ending_ = draw(
+        hypothesis.strategies.text(
+            alphabet=string.ascii_letters,
+            min_size=min_size,
+            max_size=max_size
+        )
+    )
+    return word_ending_
+
+
+def internal_link(page_name, namespace_prefix="", piped="", word_ending=""):
+    """Return internal_link.
+
+    :param str page_name: page_name
+    :param str namespace_prefix: namespace prefix
+    :param str piped: piped
+    :param str word_ending: word_ending
+
+    namespace_prefix = [ namespace ], ":";
+    piped = "|", [ anchor ];
+
+    internal_link = "[[", [ namespace_prefix ], page_name, [ piped ], "]]",
+    [ word_ending ];
+
+    :returns: internal_link
+    :rtype: str
+    """
+    internal_link = (
+        "[[{namespace_prefix}{page_name}{piped}]]{word_ending}"
+    )
+    internal_link = internal_link.format(
+        namespace_prefix=namespace_prefix,
+        page_name=page_name,
+        piped=piped,
+        word_ending=word_ending
+    )
+    return internal_link
+
+
+@hypothesis.strategies.composite
+def redirect(draw, internal_link):
+    """Return redirect.
+
+    :param str internal_link: internal_link
+
+    redirect = "#REDIRECT", space | tab, internal_link;
+
+    :returns: redirect
+    :rtype: str
+    """
+    space_tab = draw(hypothesis.strategies.sampled_from(" \t"))
+    redirect = "#REDIRECT" + space_tab + internal_link
+    return redirect
---
+++ tests/parser_elements/test_links.py
@@ -0,0 +1,95 @@
+#    This file is part of WikiPie 1.0.
+#    Copyright (C) 2018  Carine Dengler
+#
+#    WikiPie is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+"""
+:synopsis: Links parser elements tests.
+"""
+
+
+# standard library imports
+import csv
+import unittest
+
+# third party imports
+import hypothesis
+import hypothesis.strategies
+
+# library specific imports
+from src.parser_elements import links
+from tests.parser_elements import strategies
+
+
+class TestLinks(unittest.TestCase):
+    """Links parser elements tests."""
+
+    @staticmethod
+    def _get_namespaces():
+        """Get namespaces.
+
+        :returns: namespaces
+        :rtype: list
+        """
+        with open("tests/parser_elements/data/namespaces.csv") as fp:
+            reader = csv.reader(fp)
+            namespaces = next(reader)
+        return namespaces
+
+    @hypothesis.given(
+        strategies.links.namespace()
+    )
+    def test_namespace_00(self, namespace):
+        """Test namespace parser element.
+
+        :param str namespace: namespace
+        """
+        namespaces = self._get_namespaces()
+        parser_element = links._get_namespace(namespaces)
+        parse_results = parser_element.parseString(namespace)
+        self.assertEqual(namespace, parse_results["namespace"])
+        return
+
+    @hypothesis.given(
+        strategies.links.page_name(1, 16)
+    )
+    def test_page_name_00(self, page_name):
+        """Test page_name parser element.
+
+        :param str page_name: page_name
+        """
+        parser_element = links._get_page_name()
+        parse_results = parser_element.parseString(page_name)
+        self.assertEqual(page_name, parse_results["page_name"])
+        return
+
+    @hypothesis.given(
+        strategies.links.page_name(1, 16)
+    )
+    def test_internal_link_00(self, page_name):
+        """Test internal link parser element.
+
+        :param str page_name: page_name
+
+        internal_link = "[[", page_name, "]]";
+        """
+        internal_link = strategies.links.internal_link(page_name)
+        namespaces = self._get_namespaces()
+        parser_element = links.get_internal_link(namespaces)
+        parse_results = parser_element.parseString(internal_link)
+        self.assertEqual(
+            page_name, parse_results["page_name"]
+        )
+        return
